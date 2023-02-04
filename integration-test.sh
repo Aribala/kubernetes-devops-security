74b6deee85c1d615da76ae7f701f90af54058c7d
Aribala <aribala.duraipandian@gmail.com>
2023-02-04 20:53:23 +0530

    Update integration-test.sh

--- integration-test.sh
+++ integration-test.sh
@@ -10,14 +10,14 @@ echo $applicationURL:$PORT$applicationURI
 if [[ ! -z "$PORT" ]]; then
     response=$(curl -s $applicationURL:$PORT$applicationURI)
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
-    if [[ "$response" == 100]]; then
+    if [[ "$response" == 100 ]]; then
         echo "Increment Test Passed"
     else
         echo "Increment Test Failed"
         exit 1;
     fi;
 
-    if [[ "$http_code" == 200]]; then
+    if [[ "$http_code" == 200 ]]; then
         echo "Http Status Code Test Passed"
     else
         echo "Http Status Code Test Failed"
